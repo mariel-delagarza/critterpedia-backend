11618641bbc91a2c7d0c22b2863209bd64b8c01c
Mariel de la Garza <mariel.delagarza@viget.com>
2021-10-06 16:22:34 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
 const { PrismaClient } = require("@prisma/client");
-const { ApolloServer } = require("apollo-server-express");
-const express = require("express");
+const { ApolloServer } = require("apollo-server");
 const { readFileSync } = require("fs");
-require("dotenv").config();
-const resolvers = require("./resolvers");
-const cors = require("cors");
 
-const expressPlayground =
-  require("graphql-playground-middleware-express").default;
+const resolvers = require("./resolvers");
+
 const path = require("path");
+
 const prisma = new PrismaClient();
 
-const app = express();
+const server = new ApolloServer({
+  typeDefs: readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
+  resolvers,
+  context: {
+    prisma,
+  },
+  playground: true,
+  introspection: true,
+});
 
-app.use(cors());
-
-async function startServer() {
-  const server = new ApolloServer({
-    typeDefs: readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
-    resolvers,
-    context: {
-      prisma,
-    },
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
-startServer();
-
-app.get("/", (req, res) => res.end("Congrats you made it."));
-app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
-
-app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`
     🚀  Server is ready at ${url}
     📭  Query at https://studio.apollographql.com/dev
